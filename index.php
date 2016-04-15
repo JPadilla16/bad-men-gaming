73586a2156a22170c9e873e3dc0f228cdc0f6b5d
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-14 19:51:48 -0600

    Added gitignore. Added angular. Added index.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="ufc-8">
+		<title>Bad Men Gaming</title>
+	</head>
+	<body>
+		<header>
+			<h1>Bad Men Gaming</h1>
+			<p>Here are some words talking about me!</p>
+
+		</header>
+
+		<main>
+			<p>paragraph talking about shtuff</p>
+			<p>another paragraph talking about more shtuff</p>
+		</main>
+
+		<footer>
+			<div>&copy; 2016 Jared Padilla</div>
+		</footer>
+	</body>
+</html>
